b678d05cd823461ee8d8cab8d709cea1ff217f19
saad484 <aiddisaad484@gmail.com>
2024-06-15 02:12:34 +0100

    users country

--- 0x00-MySQL_Advanced/1-country_users.sql
+++ 0x00-MySQL_Advanced/1-country_users.sql
@@ -1,9 +1,9 @@
---Creates a table users
-Create TABLE IF NOT EXISTS users (
-	id INT NOT NULL AUTO_INCREMENT,
-	email VARCHAR(255) NOT NULL UNIQUE,
-	name VARCHAR(255),
-	country ENUM('US', 'CO', 'TN') NOT NULL,
-	PRIMARY KEY (id)
-	);
+-- creates a table users
+CREATE TABLE IF NOT EXISTS users (
+    id INT NOT NULL AUTO_INCREMENT,
+    email VARCHAR(255) NOT NULL UNIQUE,
+    name VARCHAR(255),
+    country ENUM('US', 'CO', 'TN') NOT NULL,
+    PRIMARY KEY (id)
+);
 
