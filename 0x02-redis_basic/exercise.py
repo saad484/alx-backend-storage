a4926aea8cbc0f4f8a17bc44bbb0b8328054e74c
saad484 <aiddisaad484@gmail.com>
2024-06-19 22:37:22 +0100

    count call replay

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -7,6 +7,53 @@ doc doc module
 import redis
 import uuid
 from typing import Union, Callable, Optional
+from functools import wraps
+
+
+def count_calls(method: Callable) -> Callable:
+    """doc doc class"""
+
+    @wraps(method)
+    def wrapper(self, *args, **kwargs):
+        """doc doc class"""
+        key = method.__qualname__
+        self._redis.incr(key)
+        return method(self, *args, **kwargs)
+
+    return wrapper
+
+
+def call_history(method: Callable) -> Callable:
+    """doc doc class"""
+    inkey = method.__qualname__ + ":inputs"
+    outkey = method.__qualname__ + ":outputs"
+
+    @wraps(method)
+    def wrapper(self, *args, **kwargs):
+        """doc doc class"""
+        self._redis.rpush(inkey, str(args))
+        res = method(self, *args, **kwargs)
+        self._redis.rpush(outkey, str(res))
+        return res
+
+    return wrapper
+
+
+def replay(method: Callable) -> None:
+    """doc doc class"""
+    input_key = "{}:inputs".format(method.__qualname__)
+    output_key = "{}:outputs".format(method.__qualname__)
+
+    inputs = method.__self__._redis.lrange(input_key, 0, -1)
+    outputs = method.__self__._redis.lrange(output_key, 0, -1)
+
+    print("{} was called {} times:".format(method.__qualname__, len(inputs)))
+    for inp, out in zip(inputs, outputs):
+        print(
+            "{}(*{}) -> {}".format(
+                method.__qualname__, inp.decode("utf-8"), out.decode("utf-8")
+            )
+        )
 
 
 class Cache():
@@ -21,6 +68,8 @@ class Cache():
         self._redis = redis.Redis()
         self._redis.flushdb()
 
+    @count_calls
+    @call_history
     def store(self, data: Union[str, bytes, int, float]) -> str:
         '''
         doc doc method
